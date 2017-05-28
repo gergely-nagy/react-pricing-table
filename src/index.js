126391dfccccb8484c9e14334846e5155aad6ace
gergely-nagy <gergely.nagy.contact@gmail.com>
2017-05-28 22:14:05 +0200

    :bookmark: Release v0.0.5

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import './index.css'
 import './skeleton.css'
+import './index.css'
 import React from 'react';
 
 class MyComponent extends React.Component {
@@ -20,9 +20,9 @@ class MyComponent extends React.Component {
       <div className="react-pricing-table">
         <div className="row">
           <div className="three columns">
-            <ul className="price">
-              <li className="header">Basic</li>
-              <li className="grey">$ 9.99 / year</li>
+            <ul className="price basic-border">
+              <li className="basic-header">GROUP</li>
+              <li className="grey">$13/month*</li>
               <li>11GB Storage</li>
               <li>10 Emails</li>
               <li>10 Domains</li>
@@ -33,9 +33,9 @@ class MyComponent extends React.Component {
             </ul>
           </div>
           <div className="three columns">
-            <ul className="price">
-              <li className="header">Basic</li>
-              <li className="grey">$ 9.99 / year</li>
+            <ul className="price highlighted-border">
+              <li className="highlighted-header">TEAM</li>
+              <li className="grey">$27/month*</li>
               <li>10GB Storage</li>
               <li>10 Emails</li>
               <li>10 Domains</li>
@@ -46,9 +46,9 @@ class MyComponent extends React.Component {
             </ul>
           </div>
           <div className="three columns">
-            <ul className="price">
-              <li className="header">Basic</li>
-              <li className="grey">$ 9.99 / year</li>
+            <ul className="price basic-border">
+              <li className="basic-header">AGENCY</li>
+              <li className="grey">$54/month*</li>
               <li>10GB Storage</li>
               <li>10 Emails</li>
               <li>10 Domains</li>
@@ -59,9 +59,9 @@ class MyComponent extends React.Component {
             </ul>
           </div>
           <div className="three columns">
-            <ul className="price">
-              <li className="header">Basic</li>
-              <li className="grey">$ 9.99 / year</li>
+            <ul className="price basic-border">
+              <li className="basic-header">ENTERPRISE</li>
+              <li className="grey">$100/month</li>
               <li>10GB Storage</li>
               <li>10 Emails</li>
               <li>10 Domains</li>
