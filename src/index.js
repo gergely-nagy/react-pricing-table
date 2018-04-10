0cfcb6d06339b1093457b3bf0abb792f3aaf4351
gergely-nagy <gergely.nagy.contact@gmail.com>
2018-04-10 20:10:29 +0200

    :wrench: Fix webpack build

--- src/index.js
+++ src/index.js
@@ -5,6 +5,12 @@ import PricingTable from './PricingTable';
 import PricingSlot from './PricingSlot';
 import PricingDetail from './PricingDetail';
 
+if (typeof window !== 'undefined') {
+  window.PricingTable = PricingTable;
+  window.PricingSlot = PricingSlot;
+  window.PricingDetail = PricingDetail;
+}
+
 export {
   PricingTable,
   PricingSlot,
