776604df982685fef9a7be22810a64202de9f2a3
Benjamin C <benjamin@coriou.net>
2019-05-15 17:39:31 +0200

    chore(enzyme): Upgrade enzyme react adapter to v16
    To match the dev version of React

--- test/Button.test.js
+++ test/Button.test.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import Button from '../src/Button';
 import { shallow, mount } from 'enzyme';
 import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-15';
+import Adapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({ adapter: new Adapter() });
 
