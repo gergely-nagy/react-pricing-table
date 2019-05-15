8041a6ce47b2bdc4a22282c3b53f2ac20a3c8fd9
Benjamin C <benjamin@coriou.net>
2019-05-16 00:44:07 +0200

    chore(prettier): Prettier all the things
    All (relevant) files went through Prettier

--- examples/Example.js
+++ examples/Example.js
@@ -1,46 +1,121 @@
-import React from 'react';
+import React from "react";
 
-import {PricingTable, PricingSlot, PricingDetail} from '../src/index';
+import { PricingTable, PricingSlot, PricingDetail } from "../src/index";
 
 class Example extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.submit = this.submit.bind(this);
-    }
+    this.submit = this.submit.bind(this);
+  }
 
-    submit(){ alert('button clicked'); }
+  submit() {
+    alert("button clicked");
+  }
 
-    render() {
-        return (
-            <PricingTable  highlightColor='#1976D2'>
-              <PricingSlot  onClick={this.submit} buttonText='TRY IT FREE' title='FREE' priceText='$0/month' onClick={this.submit}>
-                <PricingDetail> <b>15</b> projects</PricingDetail>
-                <PricingDetail> <b>5 GB</b> storage</PricingDetail>
-                <PricingDetail> <b>5</b> users</PricingDetail>
-                <PricingDetail strikethrough> <b>Time tracking</b></PricingDetail>
-              </PricingSlot>
-              <PricingSlot highlighted onClick={this.submit} buttonText='SIGN UP' title='BASIC' priceText='$24/month' onClick={this.submit}>
-                <PricingDetail> <b>35</b> projects</PricingDetail>
-                <PricingDetail> <b>15 GB</b> storage</PricingDetail>
-                <PricingDetail> <b>Unlimited</b> users</PricingDetail>
-                <PricingDetail> <b>Time tracking</b></PricingDetail>
-              </PricingSlot>
-              <PricingSlot  onClick={this.submit} buttonText='SIGN UP' title='PROFESSIONAL' priceText='$99/month' onClick={this.submit}>
-                <PricingDetail> <b>100</b> projects</PricingDetail>
-                <PricingDetail> <b>30 GB</b> storage</PricingDetail>
-                <PricingDetail> <b>Unlimited</b> users</PricingDetail>
-                <PricingDetail> <b>Time tracking</b></PricingDetail>
-              </PricingSlot>
-              <PricingSlot  onClick={this.submit} buttonText='SIGN UP' title='ENTERPRISE' priceText='$200/month' onClick={this.submit}>
-                <PricingDetail> <b>Unlimited</b> projects</PricingDetail>
-                <PricingDetail> <b>75 GB</b> storage</PricingDetail>
-                <PricingDetail> <b>Unlimited</b> users</PricingDetail>
-                <PricingDetail> <b>Time tracking</b></PricingDetail>
-              </PricingSlot>
-            </PricingTable>
-        )
-    }
+  render() {
+    return (
+      <PricingTable highlightColor="#1976D2">
+        <PricingSlot
+          onClick={this.submit}
+          buttonText="TRY IT FREE"
+          title="FREE"
+          priceText="$0/month"
+          onClick={this.submit}
+        >
+          <PricingDetail>
+            {" "}
+            <b>15</b> projects
+          </PricingDetail>
+          <PricingDetail>
+            {" "}
+            <b>5 GB</b> storage
+          </PricingDetail>
+          <PricingDetail>
+            {" "}
+            <b>5</b> users
+          </PricingDetail>
+          <PricingDetail strikethrough>
+            {" "}
+            <b>Time tracking</b>
+          </PricingDetail>
+        </PricingSlot>
+        <PricingSlot
+          highlighted
+          onClick={this.submit}
+          buttonText="SIGN UP"
+          title="BASIC"
+          priceText="$24/month"
+          onClick={this.submit}
+        >
+          <PricingDetail>
+            {" "}
+            <b>35</b> projects
+          </PricingDetail>
+          <PricingDetail>
+            {" "}
+            <b>15 GB</b> storage
+          </PricingDetail>
+          <PricingDetail>
+            {" "}
+            <b>Unlimited</b> users
+          </PricingDetail>
+          <PricingDetail>
+            {" "}
+            <b>Time tracking</b>
+          </PricingDetail>
+        </PricingSlot>
+        <PricingSlot
+          onClick={this.submit}
+          buttonText="SIGN UP"
+          title="PROFESSIONAL"
+          priceText="$99/month"
+          onClick={this.submit}
+        >
+          <PricingDetail>
+            {" "}
+            <b>100</b> projects
+          </PricingDetail>
+          <PricingDetail>
+            {" "}
+            <b>30 GB</b> storage
+          </PricingDetail>
+          <PricingDetail>
+            {" "}
+            <b>Unlimited</b> users
+          </PricingDetail>
+          <PricingDetail>
+            {" "}
+            <b>Time tracking</b>
+          </PricingDetail>
+        </PricingSlot>
+        <PricingSlot
+          onClick={this.submit}
+          buttonText="SIGN UP"
+          title="ENTERPRISE"
+          priceText="$200/month"
+          onClick={this.submit}
+        >
+          <PricingDetail>
+            {" "}
+            <b>Unlimited</b> projects
+          </PricingDetail>
+          <PricingDetail>
+            {" "}
+            <b>75 GB</b> storage
+          </PricingDetail>
+          <PricingDetail>
+            {" "}
+            <b>Unlimited</b> users
+          </PricingDetail>
+          <PricingDetail>
+            {" "}
+            <b>Time tracking</b>
+          </PricingDetail>
+        </PricingSlot>
+      </PricingTable>
+    );
+  }
 }
 
 export default Example;
