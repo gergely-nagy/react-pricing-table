4efb62dc2dbcaa5a26e79cbba590d727db32dfce
Benjamin C <benjamin@coriou.net>
2019-05-15 16:53:00 +0200

    fix(example): reverting example to original one
    Committed by accident my dev version of the example (displaying two tables instead of one to debug the issue my previous commit is fixing). This commit reverts the example to how it was

--- examples/index.js
+++ examples/index.js
@@ -1,32 +1,24 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import Example from "./Example";
-import { AppContainer } from "react-hot-loader";
+import Example from './Example';
+import { AppContainer } from 'react-hot-loader';
 // AppContainer is a necessary wrapper component for HMR
 
-const render = Component => {
-    ReactDOM.render(
-        <AppContainer>
-            <Component />
-        </AppContainer>,
-        document.getElementById("root")
-    );
+const render = (Component) => {
+  ReactDOM.render(
+    <AppContainer>
+      <Component/>
+    </AppContainer>,
+    document.getElementById('root')
+  );
 };
 
-const Examples = () => (
-    <div>
-        <Example />
-        <br />
-        <Example />
-    </div>
-);
-
-render(Examples);
+render(Example);
 
 // Hot Module Replacement API
 if (module.hot) {
-    module.hot.accept("./Example", () => {
-        render(Examples);
-    });
+  module.hot.accept('./Example', () => {
+    render(Example)
+  });
 }
