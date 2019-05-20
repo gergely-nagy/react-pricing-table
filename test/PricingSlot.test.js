b7bdb724ee300697f5127c19861ba54009b35f36
Benjamin C <benjamin@coriou.net>
2019-05-20 09:46:17 +0200

    feat(PricingSlot): custom accessors - tests
    Basic Jest test for custom accessors features: "should render without crashing"

--- test/PricingSlot.test.js
+++ test/PricingSlot.test.js
@@ -1,17 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {PricingSlot} from '../src/index';
-import { shallow } from 'enzyme';
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-15';
+import React from "react";
+import ReactDOM from "react-dom";
+import { PricingSlot } from "../src/index";
+import { shallow } from "enzyme";
+import Enzyme from "enzyme";
+import Adapter from "enzyme-adapter-react-15";
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe('PricingSlot', () => {
-    it('renders without crashing', () => {
-        const div = document.createElement('div');
+const CustomTitle = ({ id, className, highlighted, highlightColor, text }) => (
+    <div id={id} className={className}>
+        <span style={{ color: highlighted ? "white" : highlightColor }}>
+            {text}
+        </span>
+    </div>
+);
+
+const CustomPrice = ({ text }) => (
+    <div>
+        <span className="priceText">{text}</span>
+        <span className="priceOther">Could be a subtitle here</span>
+    </div>
+);
+
+describe("PricingSlot", () => {
+    it("renders without crashing", () => {
+        const div = document.createElement("div");
         ReactDOM.render(<PricingSlot />, div);
     });
 
-
+    it("renders custom accessors without crashing", () => {
+        const div = document.createElement("div");
+        ReactDOM.render(
+            <PricingSlot
+                titleAccessor={CustomTitle}
+                priceAccessor={CustomPrice}
+            />,
+            div
+        );
+    });
 });
