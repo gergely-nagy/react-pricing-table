eada4847093bdb208cae54d91bf02edaf380d771
Coriou <benjamin@coriou.net>
2019-05-20 08:58:14 +0200

    chore(Prettier): Use Prettier for code formatting (#12)

--- examples/webpack.config.babel.js
+++ examples/webpack.config.babel.js
@@ -1,45 +1,42 @@
-import path from 'path';
-import webpack from 'webpack';
-import CleanWebpackPlugin from 'clean-webpack-plugin';
+import path from "path";
+import webpack from "webpack";
+import CleanWebpackPlugin from "clean-webpack-plugin";
 
 export default () => ({
-    entry: {
-        index: path.join(__dirname, './index.js'),
-    },
+  entry: {
+    index: path.join(__dirname, "./index.js")
+  },
 
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
 
-    module: {
-        rules: [
-            {
-                test: /.jsx?$/,
-                exclude: /node_modules/,
+  module: {
+    rules: [
+      {
+        test: /.jsx?$/,
+        exclude: /node_modules/,
 
-                use: [
-                    {
-                        loader: 'babel-loader',
-                        options: {
-                            babelrc: false,
-                            presets: [
-                                ['es2015', { modules: false }],
-                                'react',
-                            ],
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.(css)$/,
-                loader: 'iso-morphic-style-loader!css-loader',
-            },
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              babelrc: false,
+              presets: [["es2015", { modules: false }], "react"]
+            }
+          }
         ]
-    },
-
-    plugins: [
-        // Clean dist folder
-        new CleanWebpackPlugin(['./dist/build.js']),
+      },
+      {
+        test: /\.(css)$/,
+        loader: "iso-morphic-style-loader!css-loader"
+      }
     ]
+  },
+
+  plugins: [
+    // Clean dist folder
+    new CleanWebpackPlugin(["./dist/build.js"])
+  ]
 });
